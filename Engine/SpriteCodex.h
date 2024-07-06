1750f832bea18778b05aeec0a3d1cad26a22832a
MrKligson <jpdenheijer@gmail.com>
2024-07-06 11:42:35 +0200

    Added spritecodex and spritesizes in codex

---
+++ Engine/SpriteCodex.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Graphics.h"
+
+class SpriteCodex
+{
+public:
+	static void DrawGameOver(int x, int y, Graphics& gfx);
+	static void DrawTitle(int x, int y, Graphics& gfx);
+	static constexpr int gameOverWidth = 83;
+	static constexpr int gameOverHeight = 63;
+	static constexpr int titleWidth = 83;
+	static constexpr int titleHeight = 63;
+};
