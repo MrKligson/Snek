68d01e38a78163799c2f42f499fb186d9fb3553f
MrKligson <jpdenheijer@gmail.com>
2024-07-06 16:34:20 +0200

    added == and + operators to Location

--- Engine/Location.h
+++ Engine/Location.h
@@ -4,4 +4,7 @@ struct Location
 {
 	int x;
 	int y;
+
+	bool operator==(const Location& rhs) const { return x == rhs.x && y == rhs.y; }
+	Location operator+(const Location& rhs) const { return { x + rhs.x, y + rhs.y }; }
 };
